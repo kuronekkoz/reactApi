a7c3445a3fc5a996dabadbf7655b04daf39d7dd8
kuronekkoz <ntra180802@gmail.com>
2025-07-08 22:40:05 +0700

    add new

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,38 @@ import './App.scss'
 import Header from './components/Header'
 import TableUsers from './components/TableUsers'
 import  Container  from 'react-bootstrap/Container';
+import ModalAddNew from './components/ModalAddNew';
+import { useState } from 'react';
 
 function App() {
 
+  const [isShowModalAddNew, setIsShowModelAddNew]= useState(false);
+  const handleClose = () =>{
+    setIsShowModelAddNew(false);
+  }
   return (
     
       <div className='app-container'>
        <Header/>
       <Container>
-     
+      <div className='my-3 add-new'>
+        <span>
+          <b>
+            List user :
+          </b>
+        </span>
+        
+        <button className='btn btn-primary' onClick={() => setIsShowModelAddNew(true)}>Add new user </button>
+      </div>
        <TableUsers/>
 
       
       </Container>
+
+      <ModalAddNew 
+        show={isShowModalAddNew}
+        handleClose={handleClose}
+      />
       </div>
     
   )
